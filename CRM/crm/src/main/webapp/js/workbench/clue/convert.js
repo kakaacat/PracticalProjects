ae584ecd2b4ba31a59c82619b9bca9f4c9f4023c
kakaacat <1912445179@qq.com>
2022-04-27 20:38:44 +0800

    crm项目--线索转换

--- CRM/crm/src/main/webapp/js/workbench/clue/convert.js
+++ CRM/crm/src/main/webapp/js/workbench/clue/convert.js
@@ -1,8 +1,8 @@
-$(function(){
-    $("#isCreateTransaction").click(function(){
-        if(this.checked){
+$(function () {
+    $("#isCreateTransaction").click(function () {
+        if (this.checked) {
             $("#create-transaction2").show(200);
-        }else{
+        } else {
             $("#create-transaction2").hide(200);
         }
     });
@@ -17,5 +17,43 @@ $(function(){
         todayBtn: true,
         clearBtn: true
     });
+
+    //给“市场活动原”添加单击事件
+    $("#activitySourceBtn").click(function () {
+        //打开模态窗口
+        $("#searchActivityModal").modal("show");
+    });
+    //给模态窗口中市场活动搜索框添加键盘抬起事件
+    $("#searchActivityTxt").keyup(function () {
+        //收集参数
+        var clueId = $("#clueIdHiddenArea").val();
+        var activityName = this.value;
+        //发送请求
+        $.ajax({
+            url: 'workbench/clue/queryActivityForConvertByANameCId.do',
+            data: {
+                activityName: activityName,
+                clueId: clueId
+            },
+            type: 'post',
+            dataType: 'json',
+            success: function (data) {
+                var htmlStr = "";
+                $.each(data, function (index, obj) {
+                    htmlStr +="<tr>";
+                    htmlStr +="<td><input value=\""+obj.id+"\" activityName=\""+obj.name+"\" type=\"radio\" name=\"activity\"/></td>";
+                    htmlStr +="<td>"+obj.name+"</td>";
+                    htmlStr +="<td>"+obj.startDate+"</td>";
+                    htmlStr +="<td>"+obj.endDate+"</td>";
+                    htmlStr +="<td>"+obj.owner+"</td>";
+                    htmlStr +="</tr>";
+                });
+                $("#tbodyClueConvert").html(htmlStr);
+            }
+        });
+    });
+
+
+
 
 });
