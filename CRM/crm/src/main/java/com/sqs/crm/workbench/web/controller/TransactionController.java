d2ad4aa751b16c061cb083c3eef941b8bb516ecb
kakaacat <1912445179@qq.com>
2022-05-16 21:49:54 +0800

    crm项目--交易

--- CRM/crm/src/main/java/com/sqs/crm/workbench/web/controller/TransactionController.java
+++ CRM/crm/src/main/java/com/sqs/crm/workbench/web/controller/TransactionController.java
@@ -1,7 +1,11 @@
 package com.sqs.crm.workbench.web.controller;
 
 import com.sqs.crm.settings.mapper.DicValueMapper;
+import com.sqs.crm.settings.mapper.UserMapper;
 import com.sqs.crm.settings.model.DicValue;
+import com.sqs.crm.settings.model.User;
+import com.sqs.crm.settings.service.DicValueService;
+import com.sqs.crm.settings.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,19 +20,37 @@ import java.util.List;
 @Controller
 public class TransactionController {
     @Autowired
-    private DicValueMapper dicValueMapper;
+    private DicValueService dicValueService;
+    @Autowired
+    private UserService userService;
 
     @RequestMapping("/workbench/transaction/index.do")
     public String index(HttpServletRequest request){
         //调用service层方法，查询数据
-        List<DicValue> transactionTypeList = dicValueMapper.selectDicValueByTypeCode("transactionType");
-        List<DicValue> sourceList = dicValueMapper.selectDicValueByTypeCode("source");
-        List<DicValue> stageList = dicValueMapper.selectDicValueByTypeCode("stage");
+        List<DicValue> transactionTypeList = dicValueService.queryDicValueByTypeCode("transactionType");
+        List<DicValue> sourceList = dicValueService.queryDicValueByTypeCode("source");
+        List<DicValue> stageList = dicValueService.queryDicValueByTypeCode("stage");
         //把数据保存到作用域
         request.setAttribute("transactionTypeList", transactionTypeList);
         request.setAttribute("sourceList", sourceList);
         request.setAttribute("stageList", stageList);
         //请求转发
         return "workbench/transaction/index";
+    }
+
+    @RequestMapping("/workbench/transaction/toSave.do")
+    public String toSave(HttpServletRequest request) {
+        //调用service层方法，查询数据
+        List<User> userList = userService.queryAllUsers();
+        List<DicValue> transactionTypeList = dicValueService.queryDicValueByTypeCode("transactionType");
+        List<DicValue> sourceList = dicValueService.queryDicValueByTypeCode("source");
+        List<DicValue> stageList = dicValueService.queryDicValueByTypeCode("stage");
+        //把数据保存到作用域
+        request.setAttribute("userList", userList);
+        request.setAttribute("transactionTypeList", transactionTypeList);
+        request.setAttribute("sourceList", sourceList);
+        request.setAttribute("stageList", stageList);
+        //请求转发
+        return "workbench/transaction/save";
     }
 }
