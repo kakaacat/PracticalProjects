54d49190f42e3d569fe3f50ff4419d983f8c8357
kakaacat <1912445179@qq.com>
2022-05-29 20:20:36 +0800

    crm项目--创建联系人

--- CRM/crm/src/main/java/com/sqs/crm/workbench/web/controller/ContactsController.java
+++ CRM/crm/src/main/java/com/sqs/crm/workbench/web/controller/ContactsController.java
@@ -1,14 +1,23 @@
 package com.sqs.crm.workbench.web.controller;
 
+import com.sqs.crm.commons.contants.Contants;
+import com.sqs.crm.commons.domain.ReturnObject;
+import com.sqs.crm.commons.utils.DateUtils;
+import com.sqs.crm.commons.utils.UUIDUtils;
 import com.sqs.crm.settings.model.DicValue;
 import com.sqs.crm.settings.model.User;
 import com.sqs.crm.settings.service.DicValueService;
 import com.sqs.crm.settings.service.UserService;
+import com.sqs.crm.workbench.model.Contacts;
+import com.sqs.crm.workbench.service.ContactsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -21,6 +30,8 @@ public class ContactsController {
     private UserService userService;
     @Autowired
     private DicValueService dicValueService;
+    @Autowired
+    private ContactsService contactsService;
 
     @RequestMapping("/workbench/contacts/index.do")
     public String index(HttpServletRequest request) {
@@ -33,5 +44,32 @@ public class ContactsController {
         request.setAttribute("sourceList", sourceList);
 
         return "workbench/contacts/index";
+    }
+
+    @RequestMapping("/workbench/contacts/saveContacts.do")
+    public @ResponseBody Object saveContacts(Contacts contacts, HttpSession session) {
+        User user = (User) session.getAttribute(Contants.SESSION_USER);
+        //封装参数
+        contacts.setId(UUIDUtils.getUUID());
+        contacts.setCreateBy(user.getId());
+        contacts.setCreateTime(DateUtils.formateDateTime(new Date()));
+
+        ReturnObject returnObject = new ReturnObject();
+        //调用service层方法
+        try {
+            int ret = contactsService.saveContacts(contacts);
+            if (ret > 0) {
+                returnObject.setCode(Contants.RETURN_OBJECT_CODE_SUCCESS);
+            } else {
+                returnObject.setCode(Contants.RETURN_OBJECT_CODE_FAIL);
+                returnObject.setMessage(Contants.RETURN_OBJECT_MESSAGE);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            returnObject.setCode(Contants.RETURN_OBJECT_CODE_FAIL);
+            returnObject.setMessage(Contants.RETURN_OBJECT_MESSAGE);
+        }
+
+        return returnObject;
     }
 }
