e2306ac5372d554c8e944e403c905f02bebacd60
kakaacat <1912445179@qq.com>
2022-05-29 17:04:53 +0800

    crm项目--创建客户

--- CRM/crm/src/main/webapp/js/workbench/customer/index.js
+++ CRM/crm/src/main/webapp/js/workbench/customer/index.js
@@ -21,6 +21,54 @@ $(function(){
         var name = $("#create-customerName").val();
         var website = $("#create-website").val();
         var phone = $("#create-phone").val();
+        var description = $("#create-describe").val();
+        var contactSummary = $("#create-contactSummary").val();
+        var nextContactTime = $("create-nextContactTime").val();
+        var address = $("#create-address1").val();
+
+        //表单验证
+        if (owner == "") {
+            alert("所有者不能为空！");
+            return;
+        }
+        if (name == "") {
+            alert("名称不能为空！")
+            return;
+        }
+        var regExp = /\d{3}-\d{8}|\d{4}-\d{7}/;
+        if (!regExp.test(phone)) {
+            alert("公司座机不合法！");
+            return;
+        }
+
+        //发送请求
+        $.ajax({
+            url: 'workbench/customer/saveCustomers.do',
+            data: {
+                owner: owner,
+                name: name,
+                website: website,
+                phone: phone,
+                description: description,
+                contactSummary: contactSummary,
+                nextContactTime: nextContactTime,
+                address: address
+            },
+            type: 'post',
+            dataType: 'json',
+            success: function (data) {
+                if (data.code == "1") {
+                    //关闭模态窗口
+                    $("#createCustomerModal").modal("hide");
+                    //刷新列表
+
+                } else{
+                    //模态窗口不关闭
+                    $("#createCustomerModal").modal("show");
+                    alert(data.message);
+                }
+            }
+        });
     });
 
 
