8691fbbe86cd22487b9f8336e7b8c2556070005c
kakaacat <1912445179@qq.com>
2022-08-24 16:21:22 +0800

    wetally

---
+++ WeTally/back/src/main/java/com/sqs/WeTallyApplication.java
@@ -0,0 +1,13 @@
+package com.sqs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeTallyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeTallyApplication.class, args);
+    }
+
+}
