4934dccdd59ea6f59bc0b9d8da3a459c71704223
kakaacat <1912445179@qq.com>
2022-09-11 20:20:39 +0800

    新增员工--异常处理

---
+++ reggie/reggie_take_out/src/main/java/com/sqs/reggie/common/GlobalExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.sqs.reggie.common;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+
+/**
+ * @Author : kaka
+ * @Date: 2022-09-11 20:10
+ * @Description 全局异常处理
+ */
+@ControllerAdvice(annotations = {RestController.class, Controller.class})
+@ResponseBody
+@Slf4j
+public class GlobalExceptionHandler {
+
+    /**
+     * 异常处理方法
+     * @return
+     */
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public R<String> exceptionHandler(SQLIntegrityConstraintViolationException e) {
+        if (e.getMessage().contains("Duplicate entry")) {
+            String[] split = e.getMessage().split(" ");
+            String msg = split[2] + "已存在";
+            return R.error(msg);
+        }
+        return R.error("新增失败");
+    }
+}
