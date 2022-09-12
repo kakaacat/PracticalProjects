cbb1f3f29fecf6af4871be987a1eeb35624c4bc3
kakaacat <1912445179@qq.com>
2022-09-12 16:34:05 +0800

    员工信息修改

--- reggie/reggie_take_out/src/main/java/com/sqs/reggie/config/WebMvcConfig.java
+++ reggie/reggie_take_out/src/main/java/com/sqs/reggie/config/WebMvcConfig.java
@@ -1,9 +1,14 @@
 package com.sqs.reggie.config;
 
+import com.sqs.reggie.common.JacksonObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
 
 /**
  * @Author : kaka
@@ -22,5 +27,19 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
         log.info("开始静态资源映射...");
         registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
         registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+    }
+
+    /**
+     * 扩展mvc框架的消息转换器
+     * @param converters
+     */
+    @Override
+    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        //创建消息转化器
+        MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
+        //设置对象转换器
+        messageConverter.setObjectMapper(new JacksonObjectMapper());
+        //将上面的消息转换器对象追加到mvc框架的转换器集合中
+        converters.add(0, messageConverter);
     }
 }
