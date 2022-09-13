5a9b02ceb94e13f45195196d170397c73ff9e4da
kakaacat <1912445179@qq.com>
2022-09-13 18:20:59 +0800

    分类信息修改

--- reggie/reggie_take_out/src/main/java/com/sqs/reggie/controller/CategoryController.java
+++ reggie/reggie_take_out/src/main/java/com/sqs/reggie/controller/CategoryController.java
@@ -60,9 +60,21 @@ public class CategoryController {
      */
     @DeleteMapping
     public R<String> delete(Long id) {
+        log.info("删除分类，id为：{}",id);
 
         categoryService.remove(id);
 
         return R.success("分类信息删除成功");
+    }
+
+    /**
+     * 根据id修改分类信息
+     * @param category
+     * @return
+     */
+    @PutMapping
+    public R<String> update(@RequestBody Category category) {
+        categoryService.updateById(category);
+        return R.success("分类信息修改成功");
     }
 }
