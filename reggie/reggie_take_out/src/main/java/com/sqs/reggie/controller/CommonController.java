191fef2953e4394ffc5cea297259f24136356118
kakaacat <1912445179@qq.com>
2022-09-14 14:03:43 +0800

    文件下载

--- reggie/reggie_take_out/src/main/java/com/sqs/reggie/controller/CommonController.java
+++ reggie/reggie_take_out/src/main/java/com/sqs/reggie/controller/CommonController.java
@@ -3,12 +3,17 @@ package com.sqs.reggie.controller;
 import com.sqs.reggie.common.R;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.UUID;
 
@@ -49,5 +54,45 @@ public class CommonController {
         }
 
         return R.success(fileName);
+    }
+
+    /**
+     * 文件下载
+     * @param name
+     * @param response
+     */
+    @GetMapping("/download")
+    public void download(String name, HttpServletResponse response) {
+        FileInputStream fileInputStream = null;
+        ServletOutputStream outputStream = null;
+
+        try {
+            fileInputStream = new FileInputStream(new File(basePath + name));
+
+            outputStream = response.getOutputStream();
+
+            response.setContentType("image/jpeg");
+
+            int len = 0;
+            byte[] bytes = new byte[1024];
+
+            while ((len = fileInputStream.read(bytes)) != -1) {
+                outputStream.write(bytes, 0, len);
+                outputStream.flush();
+            }
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            //关闭资源
+            try {
+                outputStream.close();
+                fileInputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 }
