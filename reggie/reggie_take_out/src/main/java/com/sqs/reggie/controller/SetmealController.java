63321e52fc17665f064bd52fb6850f6fb8625594
kakaacat <1912445179@qq.com>
2022-09-16 14:50:51 +0800

    套餐展示

--- reggie/reggie_take_out/src/main/java/com/sqs/reggie/controller/SetmealController.java
+++ reggie/reggie_take_out/src/main/java/com/sqs/reggie/controller/SetmealController.java
@@ -94,4 +94,22 @@ public class SetmealController {
 
         return R.success("套餐删除成功");
     }
+
+
+    /**
+     * 根据条件查询套餐数据
+     * @param setmeal
+     * @return
+     */
+    @GetMapping("/list")
+    public R<List<Setmeal>> list(@RequestBody Setmeal setmeal) {
+        LambdaQueryWrapper<Setmeal> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(setmeal.getCategoryId() != null, Setmeal::getCategoryId, setmeal.getCategoryId());
+        queryWrapper.eq(setmeal.getStatus() != null, Setmeal::getStatus, setmeal.getStatus());
+        queryWrapper.orderByDesc(Setmeal::getUpdateTime);
+
+        List<Setmeal> list = setmealService.list(queryWrapper);
+
+        return R.success(list);
+    }
 }
