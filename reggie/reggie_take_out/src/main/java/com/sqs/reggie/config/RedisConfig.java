5f39f754bf8a362a3b82a0712c484c9596ecafe8
kakaacat <1912445179@qq.com>
2022-09-20 16:21:25 +0800

    redis缓存

---
+++ reggie/reggie_take_out/src/main/java/com/sqs/reggie/config/RedisConfig.java
@@ -0,0 +1,17 @@
+package com.sqs.reggie.config;
+
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedisConfig extends CachingConfigurerSupport {
+    @Bean
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
+        //默认的Key序列化器为：JdkSerializationRedisSerializer
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(connectionFactory);
+        return redisTemplate;
+    }
+}
